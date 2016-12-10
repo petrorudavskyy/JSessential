d1dc7cb144fca32e5afaa9144fd007e1022602c3
petrorudavskyy <petruk220896@gmail.com>
2016-12-10 14:58:02 +0200

    Make changes in task 6

--- task6/script.js
+++ task6/script.js
@@ -1,29 +1,34 @@
-startGame();
-
-function startGame(){
-
-var pcNumber = Math.random()*100;
-
-	while(userNumber !== pcNumber) {
-		var userNumber = prompt ('Please, enter number' , '50');
-			
-			if (userNumber > pcNumber) {
-
-				alert('Our number is lower. Try again!');
-				break;
-			}
-
-			else if (userNumber < pcNumber) {
-
-				alert('Our number is higher. Try again!');
-				break;
-			}
-			
-			else {
-
-				alert('GREAT! You have cheats)');
-				break;
-
-			}
-		}
+start();
+//first function
+function start(){
+	//confirm if you want to play
+	var wantPlay = confirm('Do you want to play the game?');
+	//if ok -> function, if not-> alert
+	if (wantPlay) {
+		startGame();		
+	}	else {
+			alert('Ok,not today(');		
+	}
 }
+//second function
+function startGame() {
+	//random number
+	var pcNumber = Math.random()*100;
+	//user number
+	var userNumber = prompt ('Please, enter number' , '50');
+
+			if (userNumber > pcNumber) {
+				alert('Our number is lower. Try again!');
+				start();
+			}
+			else if (userNumber < pcNumber) {
+				alert('Our number is higher. Try again!');
+				start();	
+			}
+			else {
+				alert('GREAT! You have cheats)');
+				start();
+			}
+}
+
+
